2a74424f800c4a0c7d148e9a3287c150a3e19eeb
unknown <awesome@stu.ahtcm.edu.cn>
2022-08-09 11:14:19 +0800

    hello world

--- Hello.cpp
+++ Hello.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+//#include <iostream>
 
-int main()
-{
-    printf("hello world!");
+// int main()
+// {
+//     printf("hello world!");
+//     printf("HeLLO WORLD");
 
-    return 0;
-}
+//     return 0;
+// }
